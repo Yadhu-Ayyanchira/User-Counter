8b916f64c0093112ce326c6f4eadb48c7aadd2fe
Yadhu <yadhukrishnanas@gmail.com>
2023-07-21 10:04:21 +0530

    21/7/2023

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import ProfileCard from './Components/ProfileCard'
+import axios from 'axios';
 
 function App() {
+  const [count, setCount] = useState(0);
+  function addCount(){
+    setCount(count+1)
+  }
+  function decCount(){
+    setCount(count-1)
+  }
+  const fetchData =async () => {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    
+  }
+
+
   return (
     <div className="App">
       <h1>1</h1>
       <ProfileCard/>
-      <button>Prev</button>
-      <button>Next</button>
+      <button onClick={decCount}>Prev</button>
+      <button onClick={addCount}>Next</button>
     </div>
   );
 }
