a8907328aec3f9afadbb911bc8229134ce032e97
Yadhu <yadhukrishnanas@gmail.com>
2023-07-21 09:14:01 +0530

    first commit

---
+++ src/Components/ProfileCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './ProfileCard.css'
+
+function ProfileCard() {
+  return (
+    <div className='container'>
+      <div className="inner">
+        <h1>Name:</h1>
+        <p>Email:</p>
+      </div>
+    </div>
+  );
+}
+
+export default ProfileCard
